6899ea8ccd5757c5bc41bae884110436f6e9db1b
yigitozay <yigit.ozay344@gmail.com>
2023-05-27 20:25:47 +0300

    Modified the crud and added datables and swal. Changed buttons look

---
+++ LeaveManagement.Web/Configurations/MapperConfig.cs
@@ -0,0 +1,15 @@
+﻿
+using AutoMapper;
+using LeaveManagement.Web.Data;
+using LeaveManagement.Web.Models;
+
+namespace LeaveManagement.Web.Configurations
+{
+    public class MapperConfig : Profile
+
+    {
+        public MapperConfig() { 
+         CreateMap<LeaveType, LeaveTypeVM> ().ReverseMap();
+        }
+    }
+}
