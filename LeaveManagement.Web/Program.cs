afd4a5924c76861b395f6554d76a08adebea62e3
yigitozay <yigit.ozay344@gmail.com>
2023-06-01 20:56:01 +0300

    Fixed the problem that leavetype view returning error and the leave allocate button returning error

--- LeaveManagement.Web/Program.cs
+++ LeaveManagement.Web/Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddDefaultIdentity<Employee>(options => options.SignIn.RequireC
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+builder.Services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+
 
 builder.Services.AddAutoMapper(typeof(MapperConfig));
 builder.Services.AddControllersWithViews();
