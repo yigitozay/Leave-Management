26b2447276671f76ab5af868618fe397ba8a81cd
yigitozay <yigit.ozay344@gmail.com>
2023-06-09 23:51:48 +0300

    Modified the approved in MyLeave.cshtml. It shows the status now instead of returning null.

--- LeaveManagement.Web/Models/LeaveRequestVM.cs
+++ LeaveManagement.Web/Models/LeaveRequestVM.cs
@@ -1,18 +1,19 @@
 ﻿using LeaveManagement.Web.Data;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LeaveManagement.Web.Models
 {
-    public class LeaveRequestVM
+    public class LeaveRequestVM: LeaveRequestCreateVM
     {
-        public int Id { get; set; } 
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public LeaveType LeaveType { get; set; }
-        public DateTime DateRequested { get; set; }
-        public string? RequestComments { get; set; }
+        public int Id { get; set; }
+        [Display(Name = "Leave Type")]
 
-        public bool? Approved { get; set; }
+        public LeaveType LeaveType { get; set; }
+        [Display(Name = "Date Requested")]
+        public DateTime DateRequested { get; set; }
+
+        public bool?Approved { get; set; }
 
         public bool Cancelled { get; set; }
     }
